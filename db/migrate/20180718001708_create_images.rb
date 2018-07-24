136818e0b561f501b8f0918bc574c028d728d598
Miles Luders <miles.luders@appfolio.com>
2018-07-24 14:28:06 -0700

    Disable null values for Image url and title

--- db/migrate/20180718001708_create_images.rb
+++ db/migrate/20180718001708_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.text :url
-      t.text :title
+      t.text :url, null: false
+      t.text :title, null: false
 
       t.timestamps
     end
