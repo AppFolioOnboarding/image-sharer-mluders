b7b1c3ed5aa2678871546ac79c12225aaa7c7735
Miles Luders <miles.luders@appfolio.com>
2018-08-07 15:12:26 -0700

    Implement flow tests for image views

--- test/flows/images_crud_test.rb
+++ test/flows/images_crud_test.rb
@@ -1,47 +1,50 @@
 require 'flow_test_helper'
 
 class ImagesCrudTest < FlowTestCase
-  test 'add an image' do
+  def test_add_an_image
     images_index_page = PageObjects::Images::IndexPage.visit
 
     new_image_page = images_index_page.add_new_image!
 
     tags = %w[foo bar]
-    new_image_page = new_image_page.create_image!(
-      url: 'invalid',
-      tags: tags.join(', ')
-    ).as_a(PageObjects::Images::NewPage)
-    assert_equal 'must be a valid URL', new_image_page.url.error_message
+    url = 'https://media3.giphy.com/media/EldfH1VJdbrwY/200.gif'
+    new_image_page.url.set('invalid')
+    new_image_page.title.set('A random title')
+    new_image_page.tag_list.set(tags.join(', '))
+    new_image_page = new_image_page.create_image!.as_a(PageObjects::Images::NewPage)
 
-    image_url = 'https://media3.giphy.com/media/EldfH1VJdbrwY/200.gif'
-    new_image_page.url.set(image_url)
-
+    assert_text '.image_url.invalid-feedback', 'Url is not valid.'
+    new_image_page.url.set('https://media3.giphy.com/media/EldfH1VJdbrwY/200.gif')
     image_show_page = new_image_page.create_image!
-    assert_equal 'You have successfully added an image.', image_show_page.flash_message(:success)
 
-    assert_equal image_url, image_show_page.image_url
+    assert_equal 'The image has been added.', image_show_page.flash_message(:notice)
+    assert_equal url, image_show_page.image_url
     assert_equal tags, image_show_page.tags
 
     images_index_page = image_show_page.go_back_to_index!
-    assert images_index_page.showing_image?(url: image_url, tags: tags)
+    visible_image_urls = images_index_page.images.map(&:url)
+    assert_includes visible_image_urls, url
   end
 
-  test 'delete an image' do
+  def test_delete_an_image
     cute_puppy_url = 'http://ghk.h-cdn.co/assets/16/09/980x490/landscape-1457107485-gettyimages-512366437.jpg'
-    ugly_cat_url = 'http://www.ugly-cat.com/ugly-cats/uglycat041.jpg'
+    ugly_cat_url = 'https://vignette.wikia.nocookie.net/uncyclopedia/images/1/18/Ugly_cat.jpg'
     Image.create!([
-      { url: cute_puppy_url, tag_list: 'puppy, cute' },
-      { url: ugly_cat_url, tag_list: 'cat, ugly' }
+      { url: cute_puppy_url, title: 'A cute puppy', tag_list: 'puppy, cute' },
+      { url: ugly_cat_url, title: 'An ugly cat', tag_list: 'cat, ugly' }
     ])
 
     images_index_page = PageObjects::Images::IndexPage.visit
+
     assert_equal 2, images_index_page.images.count
-    assert images_index_page.showing_image?(url: ugly_cat_url)
-    assert images_index_page.showing_image?(url: cute_puppy_url)
+    visible_image_urls = images_index_page.images.map(&:url)
+    assert_includes visible_image_urls, ugly_cat_url
+    assert_includes visible_image_urls, cute_puppy_url
 
     image_to_delete = images_index_page.images.find do |image|
       image.url == ugly_cat_url
     end
+
     image_show_page = image_to_delete.view!
 
     image_show_page.delete do |confirm_dialog|
@@ -50,32 +53,42 @@ class ImagesCrudTest < FlowTestCase
     end
 
     images_index_page = image_show_page.delete_and_confirm!
-    assert_equal 'You have successfully deleted the image.', images_index_page.flash_message(:success)
 
+    assert_equal 'The image has been deleted.', images_index_page.flash_message(:notice)
     assert_equal 1, images_index_page.images.count
-    assert_not images_index_page.showing_image?(url: ugly_cat_url)
-    assert images_index_page.showing_image?(url: cute_puppy_url)
+    visible_image_urls = images_index_page.images.map(&:url)
+    assert_not_includes visible_image_urls, ugly_cat_url
+    assert_includes visible_image_urls, cute_puppy_url
   end
 
-  test 'view images associated with a tag' do
+  def test_view_images_associated_with_a_tag
     puppy_url1 = 'http://www.pawderosa.com/images/puppies.jpg'
     puppy_url2 = 'http://ghk.h-cdn.co/assets/16/09/980x490/landscape-1457107485-gettyimages-512366437.jpg'
-    cat_url = 'http://www.ugly-cat.com/ugly-cats/uglycat041.jpg'
+    cat_url = 'https://vignette.wikia.nocookie.net/uncyclopedia/images/1/18/Ugly_cat.jpg'
     Image.create!([
-      { url: puppy_url1, tag_list: 'superman, cute' },
-      { url: puppy_url2, tag_list: 'cute, puppy' },
-      { url: cat_url, tag_list: 'cat, ugly' }
+      { url: puppy_url1, title: 'Puppy 1', tag_list: 'cute, superman' },
+      { url: puppy_url2, title: 'Puppy 2', tag_list: 'cute, puppy' },
+      { url: cat_url, title: 'Ugly Cat', tag_list: 'cat, ugly' }
     ])
 
     images_index_page = PageObjects::Images::IndexPage.visit
+
+    visible_image_urls = images_index_page.images.map(&:url)
+
     [puppy_url1, puppy_url2, cat_url].each do |url|
-      assert images_index_page.showing_image?(url: url)
+      assert_includes visible_image_urls, url
     end
 
-    images_index_page = images_index_page.images[1].click_tag!('cute')
+    image_to_show = images_index_page.images.find do |image|
+      image.url == puppy_url1
+    end
+
+    image_show_page = image_to_show.view!
+    images_index_page = image_show_page.click_tag!('cute')
 
     assert_equal 2, images_index_page.images.count
-    assert_not images_index_page.showing_image?(url: cat_url)
+    visible_image_urls = images_index_page.images.map(&:url)
+    assert_not_includes visible_image_urls, cat_url
 
     images_index_page = images_index_page.clear_tag_filter!
     assert_equal 3, images_index_page.images.count
